1b7eda25377fa840de940a20ceba03f22d13f6bd
Matt <Matt@DAW>
2018-08-08 22:37:09 +1000

    updated license headers

--- src/main/java/org/openhab/binding/ipcamera/internal/MyNettyAuthHandler.java
+++ src/main/java/org/openhab/binding/ipcamera/internal/MyNettyAuthHandler.java
@@ -1,11 +1,16 @@
 /**
- * Copyright (c) 2010-2018 by the respective copyright holders.
+ * Copyright (c) 2014,2018 by the respective copyright holders.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
+
 package org.openhab.binding.ipcamera.internal;
 
 import java.security.MessageDigest;
@@ -111,9 +116,7 @@ public class MyNettyAuthHandler extends ChannelDuplexHandler {
     // First run it should not have authenticate as null
     // nonce is reused if authenticate is null so the NC needs to increment to allow this//
     public String processAuth(String authenticate, String httpMethod, String requestURI, boolean reSend) {
-
         if (authenticate != null) {
-
             if (authenticate.contains("Basic realm=\"")) {
                 if (myHandler.useDigestAuth == true) {
                     return "Error:Downgrade authenticate avoided";
